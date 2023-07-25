3f177c3cccd45f84422a3eb19aa52ed98c7e7093
jinseomun <jshin2702@naver.com>
2023-07-25 17:48:21 +0900

    <분산처리/브론즈2/X>

---
+++ Jinseo/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+    {
+        int h,m,n;
+
+        scanf("%d %d", &h, %m);
+        if (m >= 45)
+        {
+            m -= 45;
+        }
+        else
+        {
+            n = 45-m;
+            m = 60 - n;
+            if (h ! 0)
+            {
+                h -= 1;
+            }
+            else
+            {
+                h = 23;
+            }
+
+        }
+
+
+    }
